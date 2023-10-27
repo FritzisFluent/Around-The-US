cc0a50d90e8210c9b5d8abf2c48c6ca299826bae
Fritz Gerald Alexandre <fritzgeraldalexandre7@gmail.com>
2023-10-27 01:57:17 -0400

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -31,8 +31,9 @@ const initialCards = [
     link: "https://images.unsplash.com/photo-1515112569565-1e4aef316db9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
 ];
-
-/* ------------Elements----------- */
+/* ------------------------------------ */
+/* ------------Elements---------------- */
+/* ------------------------------------ */
 //!Select the modal, profile edit & modal__close button using their id.
 //!Select the HTML profile elements and form elements displaying current values.
 const profileEditButton = document.querySelector("#profile-edit-button");
@@ -46,32 +47,41 @@ const modalFormInputDescription = document.querySelector(
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
 
-/* ------------Functions----------- */
-//! Reuseable Callback function to close the modal.
+/* ------------------------------------- */
+/* ------------Functions--------------- */
+/* ------------------------------------ */
+//! Reuseable Callback functions to close the modal.
 function closePopup() {
   profileEditModal.classList.remove("modal__opened");
 }
 
+function UpdateFormField() {
+  profileEditModal.classList.add("modal__opened");
+  modalFormInputTitle.value = profileTitle.textContent;
+  modalFormInputDescription.value = profileDescription.textContent;
+}
+
+/* ------------------------------------- */
 /* ------------Event Handlers----------- */
-//!Prevent the page to reload after form submission.
-profileEditForm.addEventListener("submit", (evt) => {
+/* ------------------------------------- */
+
+function handleFormSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = modalFormInputTitle.value;
   profileDescription.textContent = modalFormInputDescription.value;
   closePopup();
-});
+}
 
+/* ------------------------------------- */
 /* ------------EventListeners----------- */
+/* ------------------------------------- */
+
 //! Add an event listener to the profile edit button.
-//! Add an eventListener to the moda__close button.
 //!Set the modal button to open upon clicking.
 //!Set the value of the form inputs to the profile elements text contents.
-//!set the modal_close button to close upon clicking.
-profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal__opened");
-  modalFormInputTitle.value = profileTitle.textContent;
-  modalFormInputDescription.value = profileDescription.textContent;
-});
-modalCloseButton.addEventListener("click", () => {
-  closePopup();
-});
+
+profileEditButton.addEventListener("click", UpdateFormField);
+modalCloseButton.addEventListener("click", closePopup);
+
+//!Prevent the page to reload after form submission.
+profileEditForm.addEventListener("submit", handleFormSubmit);
