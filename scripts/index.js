18962abaefdb061cda35ef10bb7931a43221a7c5
Fritz Gerald Alexandre <fritzgeraldalexandre7@gmail.com>
2023-10-29 18:29:08 -0400

    rendered cards thru template element

--- scripts/index.js
+++ scripts/index.js
@@ -27,7 +27,7 @@ const initialCards = [
   },
   //!Object6
   {
-    name: "BronwStone",
+    name: "Bronwstone",
     link: "https://images.unsplash.com/photo-1515112569565-1e4aef316db9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
   },
 ];
@@ -46,6 +46,9 @@ const modalFormInputDescription = document.querySelector(
   "#form-input-description"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardList = document.querySelector(".cards__list");
+const carTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 /* ------------------------------------- */
 /* ------------Functions--------------- */
@@ -54,17 +57,42 @@ const profileEditForm = profileEditModal.querySelector(".modal__form");
 function closePopup() {
   profileEditModal.classList.remove("modal__opened");
 }
-
+//! Callback functions to update the form field upon opening the Modal.
 function UpdateFormField() {
   profileEditModal.classList.add("modal__opened");
   modalFormInputTitle.value = profileTitle.textContent;
   modalFormInputDescription.value = profileDescription.textContent;
+}
+//! This ECMAScript 6 (ES6) Function works too.
+/*const UpdateFormField = () => {
+  profileEditModal.classList.add("modal__opened");
+  modalFormInputTitle.value = profileTitle.textContent;
+  modalFormInputDescription.value = profileDescription.textContent;
+}*/
+
+function getCardElement(cardData) {
+  // Declare a getCardElement() function with one parameter named data. You’ll be passing objects of the array to it. The function should:
+  //clone the template element with all its content and store it in a cardElement variable.
+  const cardElement = carTemplate.cloneNode(true);
+  //access the card title and image and store them in variables.
+  const cardTitleElement = cardElement.querySelector(".card__title");
+  const cardImageElement = cardElement.querySelector(".card__image");
+  // Set the path to the image to the link field of the object
+  cardImageElement.src = cardData.link; // Set the image source
+  // Set the image alt text to the name field of the object
+  cardImageElement.alt = cardData.name; // Set the image alt attribute
+  //set the card title to the name field of the object, too
+  cardTitleElement.textContent = cardData.name;
+  //return the ready HTML element with the filled-in data.
+  return cardElement;
 }
 
 /* ------------------------------------- */
 /* ------------Event Handlers----------- */
 /* ------------------------------------- */
 
+//! Function/Event Handler to prevent the page to reload upon submiting
+//! Update the profile elements with the form fiels value upon submitting
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = modalFormInputTitle.value;
@@ -85,3 +113,8 @@ modalCloseButton.addEventListener("click", closePopup);
 
 //!Prevent the page to reload after form submission.
 profileEditForm.addEventListener("submit", handleFormSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.prepend(cardElement);
+});
